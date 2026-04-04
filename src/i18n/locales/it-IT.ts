980d9d1160f6ba045bc48621a284f9694c6c2116
saberzero1 <github@emilebangma.com>
2026-04-04 02:13:24 +0200

    i18n: add content-index locales from Quartz v4

---
+++ src/i18n/locales/it-IT.ts
@@ -0,0 +1,9 @@
+export default {
+  pages: {},
+  components: {
+    rss: {
+      recentNotes: "Note recenti",
+      lastFewNotes: ({ count }: { count: number }) => (count === 1 ? "Ultima nota" : `Ultime ${count} note`),
+    },
+  },
+};
